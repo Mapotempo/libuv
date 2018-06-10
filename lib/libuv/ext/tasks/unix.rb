0492f290e55021815d7843f25d10b21bf55674c4
Stephen von Takach <steve@cotag.me>
2018-06-10 14:43:02 +1000

    (build) revert changes to unix build

--- lib/libuv/ext/tasks/unix.rb
+++ lib/libuv/ext/tasks/unix.rb
@@ -1,20 +1,39 @@
 # frozen_string_literal: true
 
-file "ext/libuv/.libs/libuv.1.#{FFI::Platform::LIBSUFFIX}" => 'ext/libuv/build' do
+file 'ext/libuv/build/gyp' => 'ext/libuv/build' do
+    result = true
+    if not File.directory?('ext/libuv/build/gyp')
+        result = system "git", "clone", "https://chromium.googlesource.com/external/gyp", "ext/libuv/build/gyp"
+    end
+    raise 'unable to download gyp' unless result
+end
+
+CLEAN.include('ext/libuv/build/gyp')
+
+file 'ext/libuv/out' => 'ext/libuv/build/gyp' do
+    target_arch = 'ia32'if FFI::Platform.ia32?
+    target_arch = 'x64' if FFI::Platform.x64?
+
+    abort "Don't know how to build on #{FFI::Platform::ARCH} (yet)" unless target_arch
+
     Dir.chdir("ext/libuv") do |path|
-        system "sh", "autogen.sh"
-        system "./configure"
-        system "make"
+        system "./gyp_uv.py -f make -Dtarget_arch=#{target_arch} -Duv_library=shared_library -Dcomponent=shared_library"
     end
 end
 
-file "ext/libuv/lib/libuv.#{FFI::Platform::LIBSUFFIX}" => "ext/libuv/.libs/libuv.1.#{FFI::Platform::LIBSUFFIX}" do
-    FileUtils.mkdir('ext/libuv/lib') unless File.directory?('ext/libuv/lib')
-    FileUtils.cp("ext/libuv/.libs/libuv.1.#{FFI::Platform::LIBSUFFIX}", "ext/libuv/lib/libuv.#{FFI::Platform::LIBSUFFIX}")
-
+file "ext/libuv/out/Release/lib.target/libuv.#{FFI::Platform::LIBSUFFIX}" => 'ext/libuv/out' do
     Dir.chdir("ext/libuv") do |path|
-        system "make", "install"
+        system 'make -C out BUILDTYPE=Release'
     end
 end
 
-CLOBBER.include("ext/libuv/.libs/libuv.1.#{FFI::Platform::LIBSUFFIX}")
+file "ext/libuv/lib/libuv.#{FFI::Platform::LIBSUFFIX}" => "ext/libuv/out/Release/lib.target/libuv.#{FFI::Platform::LIBSUFFIX}" do
+    begin
+        FileUtils.cp("ext/libuv/out/Release/lib.target/libuv.#{FFI::Platform::LIBSUFFIX}", "ext/libuv/lib/libuv.#{FFI::Platform::LIBSUFFIX}")
+    rescue => e
+        FileUtils.cp("ext/libuv/out/Release/lib.target/libuv.#{FFI::Platform::LIBSUFFIX}.1", "ext/libuv/lib/libuv.#{FFI::Platform::LIBSUFFIX}")
+    end
+end
+
+CLEAN.include('ext/libuv/out')
+CLOBBER.include("ext/libuv/out/Release/lib.target/libuv.#{FFI::Platform::LIBSUFFIX}")
